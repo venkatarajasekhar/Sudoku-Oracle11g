693fd9bbf9f1f63cec245d7b6751fbf1f27ed499
Daniel Hernandez <daniel95.hg@gmail.com>
2015-12-03 08:55:23 -0600

    Display Game
    el script display_game.sql funciona a la perfeccion para el juego dummy
    numero 1.

--- core/display_game.sql
+++ core/display_game.sql
@@ -1,12 +1,31 @@
 DECLARE
+	x number := 1;
+	y number := 3;
+	counter number := 1;
 	linea varchar2(999);
-	CURSOR juego IS (SELECT * from casillas where idJuego = 1);
 BEGIN
-	FOR i in 1..2 LOOP
-		FOR casilla in (select * from casillas where fila = i) LOOP
-			linea := linea || '|' || casilla.numero || '|';
+	dbms_output.put_line('---------------------------------------------');
+	FOR i IN 1..3 LOOP
+		FOR j in 1..3 LOOP
+			FOR casilla in (select * from casillas where fila = j and idJuego = 1 and
+								matriz between x and y order by matriz, fila, columna) LOOP
+				IF (counter = 9) THEN
+					linea := linea || ' | ' || casilla.numero || ' | ';
+				ELSIF (MOD(counter,3) = 0) THEN
+					linea := linea || ' | ' || casilla.numero || ' | -';
+				ELSE
+					linea := linea || ' | ' || casilla.numero;
+				END IF;
+				counter := counter + 1;
+			END LOOP;
+			dbms_output.put_line(linea);
+			dbms_output.new_line();
+			linea := '';
+			counter := 1;
 		END LOOP;
-		dbms_output.put_line(linea);
+		x := y + 1;
+		y := y + 3;
+		dbms_output.put_line('---------------------------------------------');
 	END LOOP;
 END;
 /
