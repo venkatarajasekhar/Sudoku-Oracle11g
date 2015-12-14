3642817b919190a03d1d116ae15172a142b7e3e3
felipe0610 <fcalderoncas@gmail.com>
2015-12-13 23:26:56 -0600

    Validacion Casilla Inicial

--- core/ingresar_numero.sql
+++ core/ingresar_numero.sql
@@ -3,12 +3,22 @@ DECLARE
 	pfila number(1) := &fila;
 	pcolumna number(1) := &columna;
 	pnumero number(1) := &numero;
+	cambiable number(1);
 BEGIN
-	IF numerovalido(pmatriz, pfila, pcolumna,pnumero,:juego_actual) THEN
+
+	
+	cambiable := select casillas.cambiable from casillas where matriz = pmatriz 
+		and columna = pcolumna and fila = pfila and idJuego = :juego_actual;
+		
+	/*Revisa si la casilla que se desea cambiar es una casilla inicial*/
+	IF cambiable = 0 THEN 
+		dbms_output.put_line('No es posible cambiar esta casilla, intente de nuevo');
+
+	ELSIF numerovalido(pmatriz, pfila, pcolumna,pnumero,:juego_actual) THEN
 		UPDATE casillas
 		set numero = pnumero
 		where matriz = pmatriz and columna = pcolumna
-		and fila = pfila;
+		and fila = pfila and idJuego = :juego_actual;
 	ELSE
 		dbms_output.put_line('No se ha podido ingresar el numero, 
 			intente de nuevo');
